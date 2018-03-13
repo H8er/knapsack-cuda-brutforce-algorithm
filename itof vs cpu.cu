a1a196400b5780a0b27d13f5f4812b23dbe65fd8
H8er <32039960+H8er@users.noreply.github.com>
2018-03-13 09:50:46 +0300

    Update itof vs cpu.cu

--- itof vs cpu.cu
+++ itof vs cpu.cu
@@ -52,9 +52,10 @@ __syncthreads ();
       __syncthreads ();
     }
   // write result for this block to global mem
+  if(threadIdx.x == 0){
   sh_sum_dev[blockIdx.x+max_blocks*rep] = sh_maxs[0];
   str_num_dev[blockIdx.x+max_blocks*rep] = indices[0] + blockIdx.x * blockDim.x +max_blocks*rep;
-}
+}}
 
 __global__ void
 reduction_max (float *s, float *str_num_dev)
@@ -103,12 +104,12 @@ int main(){
   cudaEvent_t start, stop;
 cudaEventCreate(&start);
 cudaEventCreate(&stop);
-
+float milliseconds1 = 0;
+float milliseconds2 = 0;
   long int strSize_b = pow (2, arraySize);
   int num_of_blocks = strSize_b / threads_per_block;
   float *Sum = new float[1];	// = { 0 };
   float *sh_sum_dev;
-  float *weight_dev;
   float weight[31] ={ 5, 10, 17, 19, 20, 23, 26, 30, 32, 38, 40, 44, 47, 50, 55, 56, 56, 60, 62, 66, 70, 75, 77, 80, 81, 90,93,96,101,107,115 };
   float values[31] ={ 10, 13, 16, 22, 30, 25, 55, 90, 110, 115, 130, 120, 150, 170, 194, 199, 194, 199, 217, 230, 248, 250, 264, 271, 279, 286,293,299,305,313,321 };
   float dev_coefs[62] = {5, 10, 17, 19, 20, 23, 26, 30, 32, 38, 40, 44, 47, 50, 55, 56, 56, 60, 62, 66, 70, 75, 77, 80, 81, 90,93,96,101,107,115, 10, 13, 16, 22, 30, 25, 55, 90, 110, 115, 130, 120, 150, 170, 194, 199, 194, 199, 217, 230, 248, 250, 264, 271, 279, 286,293,299,305,313,321 };
@@ -118,18 +119,18 @@ cudaEventCreate(&stop);
   long sec, usec;
   float *str_num_dev;
   float *str_num = new float[1];
+  float N_of_rep;
+  N_of_rep = num_of_blocks/max_blocks;
 
   cout <<"N of items "<<arraySize<<"\n";
   cout<<"N of blocks "<<num_of_blocks<<"\n";
   cout<<"strSize_b = "<<strSize_b<<"\n";
   cout<<"num_of_blocks / threads_per_block = "<<num_of_blocks / threads_per_block<<"\n";
-  cout<<"IDEA  "<<strSize_b/num_of_blocks<<"\n";
-  cout<<"red param "<<num_of_blocks / threads_per_block<<"  ,  "<<strSize_b/num_of_blocks<<"\n";
-
-  float N_of_rep;
-  N_of_rep = num_of_blocks/max_blocks;
   cout<<"N of repeats = "<<N_of_rep<<"\n";
   cout<<"sing param = "<<num_of_blocks/N_of_rep<<" _ "<< threads_per_block<<"\n";
+  cout<<"red param "<<num_of_blocks / threads_per_block<<"  ,  "<<strSize_b/num_of_blocks<<"\n";
+
+
   gettimeofday (&t0, NULL);
   cudaMalloc ((void **) &sh_sum_dev,  num_of_blocks * sizeof (float));
 
@@ -145,24 +146,29 @@ int sing_blocks = num_of_blocks/N_of_rep;
              }
 cudaEventRecord(stop);
 cudaEventSynchronize(stop);
-float milliseconds = 0;             
-cudaEventElapsedTime(&milliseconds, start, stop);
+
+cudaEventElapsedTime(&milliseconds1, start, stop);
+cudaEventRecord(start);
 int k = num_of_blocks/threads_per_block;
 while(k>1){
-  cout<<k<<"  "<<threads_per_block<<"\n";
+
                reduction_max <<<k, threads_per_block>>> (sh_sum_dev, str_num_dev);
                if(k>=threads_per_block){k/=threads_per_block;}
                else break;
              }
-cout<<k<<"\n";
-reduction_max <<<1,k>>> (sh_sum_dev, str_num_dev);
 
+reduction_max <<<1,k>>> (sh_sum_dev, str_num_dev);
+cudaEventSynchronize(stop);
+
+cudaEventElapsedTime(&milliseconds2, start, stop);
   cudaMemcpy (Sum, sh_sum_dev, sizeof (float), cudaMemcpyDeviceToHost);
   cudaMemcpy (str_num, str_num_dev, sizeof (float), cudaMemcpyDeviceToHost);
 	gettimeofday (&t1, 0);
 	sec = (t1.tv_sec - t0.tv_sec);
   usec = t1.tv_usec - t0.tv_usec;
   cout << "GPU time = " << sec << " sec, " << usec << " microsec\n";
+  cout<<"single_thread time = "<<milliseconds1/1000.0<<"sec\n";
+  cout<<"reduction time = "<<milliseconds2/1000.0<<"sec\n";
 
   cout << "Acheived maximal sum = " << Sum[0] << "\n";
   cout << "String number " << str_num[0] << "\n";
